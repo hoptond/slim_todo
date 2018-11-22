fca059a5905be6b099c21dd5be17c8832a221b9c
Daniel <hoptond848@protonmail.com>
2018-11-22 15:20:58 +0000

    add TaskListController class

---
+++ src/classes/Controllers/TaskListController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Todo\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class TaskListController
+{
+    private $logger;
+    private $renderer;
+    private $taskList;
+
+    public function __construct($logger, $renderer, $tasklist)
+    {
+        $this->logger = $logger;
+        $this->renderer = $renderer;
+        $this->taskList = $tasklist;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $this->logger->info("Slim-Skeleton '/' route");
+        $args['tasklist'] = $this->get('TaskList');
+        // Render index view
+        return $this->renderer->render($response, 'index.phtml', $args);
+    }
+
+}
