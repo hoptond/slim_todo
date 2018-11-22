a2f01522ab51391a45536a1368d05bc88403c6ef
Daniel <hoptond848@protonmail.com>
2018-11-22 15:07:11 +0000

    add getter for task description

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -8,4 +8,11 @@ class Task
     private $desc;
     private $status;
 
+    /**
+     * @return string Returns the description for this task.
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
 }
