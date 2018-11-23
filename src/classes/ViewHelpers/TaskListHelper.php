2aadce1d88ed859445553cfd462684deb2ce178f
Daniel <hoptond848@protonmail.com>
2018-11-23 10:47:41 +0000

    seperate input element into its own method in TaskListHelper

--- src/classes/ViewHelpers/TaskListHelper.php
+++ src/classes/ViewHelpers/TaskListHelper.php
@@ -4,16 +4,23 @@ namespace Todo\ViewHelpers;
 
 class TaskListHelper
 {
+    
     public static function ListTasks($tasks)
     {
         $output = '';
         foreach ($tasks as $task)
         {
             $output .= '<li>';
-            $output .= '<input type="checkbox" id="'. $task->getId() . '" name="'. $task->getId() .  '"'. self::DisplayTaskStatus($task->getStatus()) . '><span>' . $task->getDesc() . '</span>';
+            $output .= self::DisplayInput($task);
+            $output .= '<span>' . $task->getDesc() . '</span>';
             $output .= '</li>';
         }
         return $output;
+    }
+
+    public static function DisplayInput($task)
+    {
+        return '<input type="checkbox" id="'. $task->getId() . '" name="'. $task->getId() .  '"'. self::DisplayTaskStatus($task->getStatus()) . '>';
     }
 
     public static function DisplayTaskStatus($status)
